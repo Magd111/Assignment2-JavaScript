a0075a465c8df160ff7ab5a54423fd3e74279680
Magd111 <magdkoraby@gmail.com>
2024-11-10 21:27:00 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,80 @@
+
+function getquotes(){
+    var quotes =[{
+                    "name":"Epictetus",
+                    "Detail":"It's not what happens to you, but how you react to it that matters."
+                },
+                {
+                    "name":"Nelson Mandela",
+                    "Detail":"Resentment is like drinking poison and waiting for your enemies to die."
+                },
+                {
+                    "name":"Elbert Hubbard",
+                    "Detail":"Do not take life too seriously. You will not get out alive."
+                },
+                {
+                    "name":"Frank Sinatra",
+                    "Detail":"The best revenge is massive success."
+                },
+                {
+                    "name":"Wayne Gretzy",
+                    "Detail":"You miss 100% of the shots you don\'t take."
+                },
+                {
+                    "name":"Jim Rohn",
+                    "Detail":"Beware of what you become in pursuit of what you want."
+                }
+    ]
+    var random = parseInt(Math.random()*quotes.length);
+    document.getElementById("demo2").innerHTML=`"${quotes[random].Detail}"`;
+    document.getElementById("demo").innerHTML= `--${quotes[random].name}`;
+}
+
+
+
+
+
+//للترتيب
+
+// var quotes =[{
+//     "name":"Epictetus",
+//     "Detail":"It's not what happens to you, but how you react to it that matters."
+// },
+// {
+//     "name":"Nelson Mandela",
+//     "Detail":"Resentment is like drinking poison and waiting for your enemies to die."
+// },
+// {
+//     "name":"Elbert Hubbard",
+//     "Detail":"Do not take life too seriously. You will not get out alive."
+// },
+// {
+//     "name":"Frank Sinatra",
+//     "Detail":"The best revenge is massive success."
+// },
+// {
+//     "name":"Wayne Gretzy",
+//     "Detail":"You miss 100% of the shots you don\'t take."
+// },
+// {
+//     "name":"Jim Rohn",
+//     "Detail":"Beware of what you become in pursuit of what you want."
+// }
+// ];
+
+// var order = 0;
+
+// function getquotes() {
+//     if (order < quotes.length) {
+//         document.getElementById("demo2").innerHTML = `"${quotes[order].Detail}"`;
+//         document.getElementById("demo").innerHTML = `--${quotes[order].name}`;
+//         order++;
+//     } else {
+//         order = 0;
+//         document.getElementById("demo2").innerHTML = `"${quotes[order].Detail}"`;
+//         document.getElementById("demo").innerHTML = `--${quotes[order].name}`;
+//     }
+// }
+
+
+
