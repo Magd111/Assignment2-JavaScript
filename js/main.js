a9991ed15f508f93e0499bf2c0595ceee1b54ae2
Magd111 <magdkoraby@gmail.com>
2024-11-28 20:37:10 +0200

    sec commit

--- js/main.js
+++ js/main.js
@@ -1,31 +1,39 @@
-
+var quotes =[{
+            "name":"Epictetus",
+            "Detail":"It's not what happens to you, but how you react to it that matters."
+        },
+        {
+            "name":"Nelson Mandela",
+            "Detail":"Resentment is like drinking poison and waiting for your enemies to die."
+        },
+        {
+            "name":"Elbert Hubbard",
+            "Detail":"Do not take life too seriously. You will not get out alive."
+        },
+        {
+            "name":"Frank Sinatra",
+            "Detail":"The best revenge is massive success."
+        },
+        {
+            "name":"Wayne Gretzy",
+            "Detail":"You miss 100% of the shots you don\'t take."
+        },
+        {
+            "name":"Jim Rohn",
+            "Detail":"Beware of what you become in pursuit of what you want."
+        }
+]
+var lastRandom = null;
+var random = 0;
 function getquotes(){
-    var quotes =[{
-                    "name":"Epictetus",
-                    "Detail":"It's not what happens to you, but how you react to it that matters."
-                },
-                {
-                    "name":"Nelson Mandela",
-                    "Detail":"Resentment is like drinking poison and waiting for your enemies to die."
-                },
-                {
-                    "name":"Elbert Hubbard",
-                    "Detail":"Do not take life too seriously. You will not get out alive."
-                },
-                {
-                    "name":"Frank Sinatra",
-                    "Detail":"The best revenge is massive success."
-                },
-                {
-                    "name":"Wayne Gretzy",
-                    "Detail":"You miss 100% of the shots you don\'t take."
-                },
-                {
-                    "name":"Jim Rohn",
-                    "Detail":"Beware of what you become in pursuit of what you want."
-                }
-    ]
-    var random = parseInt(Math.random()*quotes.length);
+    do{
+        random = parseInt(Math.random()*quotes.length);
+    }
+    while(random == lastRandom) //لو كان الرقم نفسه فارجع جيب رقم تاني ... و لو عدد أخر ف خزنه في lastRandom
+        lastRandom = random;
+
+
+    console.log(random);
     document.getElementById("demo2").innerHTML=`"${quotes[random].Detail}"`;
     document.getElementById("demo").innerHTML= `--${quotes[random].name}`;
 }
